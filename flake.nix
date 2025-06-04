bc65cba022a04d88c00f94b5f824a70a44c41ff5
Matthias Beyer <mail@beyermatthias.de>
2025-06-04 20:29:26 +0200

    Add lib
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- flake.nix
+++ flake.nix
@@ -16,7 +16,54 @@
       };
     in
     {
-      packages = {
+      lib.mkLib = { pkgs, mirroritPackage }: let
+        mkMirror = {
+          name,
+          repo_url,
+          destination_path,
+          enable ? true,
+          timer ? true,
+          ...
+        }: {
+          service = {
+            "mirrorit-${name}" = {
+              inherit enable;
+              description = "mirroring service for ${repo_url}";
+
+              requires = ["network.target"];
+
+              script = ''
+                ${pkgs.bash}/bin/bash ${mirroritPackage}/bin/mirrorit "${repo_url}" "${destination_path}"
+              '';
+            };
+          };
+
+          timer = {
+            "mirrorit-${name}-timer" = {
+              inherit enable;
+
+              description = "trigger for mirroring service for ${repo_url}";
+              timerConfig = {
+                OnCalendar = "weekly";
+                Unit = "mirrorit-${name}.service";
+                RandomizedDelaySec = "1200";
+              };
+
+              requires = ["network.target"];
+              wantedBy = ["timers.target"];
+            };
+          };
+        };
+      in
+      {
+        inherit
+          mkMirror
+          ;
+      };
+
+      packages = rec {
+        default = mirrorit;
+
         mirrorit = pkgs.writeShellApplication {
           name = "mirrorit";
           text = builtins.readFile ./mirrorit;
